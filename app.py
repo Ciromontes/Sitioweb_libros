4cc14df73c73acda91cb110e00a15635935aed74
Ciromontes <ciromontes25@hotmail.com>
2022-09-13 10:26:45 -0500

    Creando la Estructura Del Administrador

--- app.py
+++ app.py
@@ -15,5 +15,13 @@ def libros():
 def nosotros():
     return render_template('sitio/nosotros.html')
 
+@app.route('/admin/')
+def admin_index():
+    return render_template('admin/index.html')
+
+@app.route('/admin/login')
+def admin_login():
+    return render_template('admin/login.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
