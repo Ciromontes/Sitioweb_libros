bd71360db0639c7b97fd305990a17065cf88eaf5
Ciromontes <ciromontes25@hotmail.com>
2022-09-12 19:38:57 -0500

    Cabecera y pie de página

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app=Flask(__name__)
 def inicio():
     return render_template('sitio/index.html')
 
+@app.route('/libros')
+def libros():
+    return render_template('sitio/libros.html')
+
+@app.route('/nosotros')
+def nosotros():
+    return render_template('sitio/nosotros.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
